896debcebdb60e474532270942946f772de74b6c
Ashiq Rahman <0xashiqrahman@gmail.com>
2023-06-23 16:41:10 +0530

    typo fixed

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subent" "public" {
+resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.101.0/24"
     tags = {
